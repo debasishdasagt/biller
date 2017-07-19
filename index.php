ee3f063ea075d611f4efb107d4bd77938bd08234
debasishdasagt <debasishdasagt@users.noreply.github.com>
2017-07-20 00:12:21 +0530

    mailer

--- index.php
+++ index.php
@@ -18,7 +18,24 @@ if(!$db)
         <br><br><br><br><br><br>
         <img src="images/flight.png">
         <br><br>
-        <div id="loginbox"></div>
+        <div id="loginbox">
+            <br>
+            <table width="80%" border="0" cellpadding="5">
+                <tr>
+                    <td>Username</td>
+                    <td><input type="text" id="uname"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" id="passwd"></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <div class="btn1">Login</div>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </center>
     </body>
 </html>
--- mailer.php
+++ mailer.php
@@ -1,77 +1,27 @@
 <?php
-/**
- * This example shows settings to use when sending via Google's Gmail servers.
- */
-
-//SMTP needs accurate times, and the PHP time zone MUST be set
-//This should be done in your php.ini, but this is how to do it if you don't have access to that
-//date_default_timezone_set('Etc/UTC');
-
-require "PHPMailer/PHPMailerAutoload.php";
-
-//Create a new PHPMailer instance
-$mail = new PHPMailer;
-
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
-
-//Ask for HTML-friendly debug output
-$mail->Debugoutput = 'html';
-
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
-
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
-
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'tls';
-
-//Whether to use SMTP authentication
+error_reporting(E_ALL);
+require("PHPMailer_5.2.4/class.phpmailer.php");
+$mail = new PHPMailer();
+$mail->IsSMTP(); // set mailer to use SMTP
+$mail->SMTPDebug  = 0;
+$mail->From = "debasishdas.trp@gmail.com";
+$mail->FromName = "Debasish Das";
+$mail->Host = "smtp.gmail.com"; // specif smtp server
+$mail->SMTPSecure= "ssl"; // Used instead of TLS when only POP mail is selected
+$mail->Port = 465; // Used instead of 587 when only POP mail is selected
 $mail->SMTPAuth = true;
+$mail->Username = "debasishdas.trp@gmail.com"; // SMTP username
+$mail->Password = "debasishdev"; // SMTP password
+$mail->AddAddress("debasishdas.agt@gmail.com", "Debasish"); //replace myname and mypassword to yours
+$mail->AddReplyTo("rlama.agt@gmail.com", "Radhika");
+$mail->WordWrap = 50; // set word wrap
+//$mail->AddAttachment("c:\\temp\\js-bak.sql"); // add attachments
+//$mail->AddAttachment("c:/temp/11-10-00.zip");
 
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "debasishdas.trp@gmail.com";
+$mail->IsHTML(true); // set email format to HTML
+$mail->Subject = 'test';
+$mail->Body = 'test';
 
-//Password to use for SMTP authentication
-$mail->Password = "debasishdev";
-
-//Set who the message is to be sent from
-$mail->setFrom('debasishdas.trp@gmail.com', 'Debasish Das');
-
-//Set an alternative reply-to address
-$mail->addReplyTo('rlama.agt@gmail.com', 'Debasish Das');
-
-//Set who the message is to be sent to
-$mail->addAddress('debasishdas.agt@gmail.com', 'DEV');
-
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->Body = "Dummy Body";
-
-//Replace the plain text body with one created manually
-//$mail->AltBody = 'This is a plain-text message body';
-
-//Attach an image file
-//$mail->addAttachment();
-
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
-
-
+if($mail->Send()) {echo "Send mail successfully";}
+else {echo "Send mail fail";}
+?>
