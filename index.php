8faac347c9614fc630ed6e31684f796fc97e3aad
debasishdasagt <debasishdasagt@users.noreply.github.com>
2017-07-21 08:04:24 +0530

    kkk

--- index.php
+++ index.php
@@ -7,7 +7,33 @@ if(!$db)
 {
     echo $db->lastErrorMsg();
 }
- else {?>
+ else {
+     
+     if(isset($_POST['uname']))
+     {
+         if(isset($_POST['passwd']))
+         {
+             $loginsql="select count(*) from users where usern='".$_POST['uname']."' and passwd='".$_POST['passwd']."';";
+             $loginres=$db->querySingle($loginsql);
+             if($loginres >> 0)
+             {
+                header("location: home.php"); 
+             }
+             else
+             {
+                 header("location: index.php?e=Username or password is incorrect");
+             }
+             
+         }
+         else
+         {
+             header("location: index.php?e=Please enter a password");
+         }
+     }
+     
+     
+     
+     ?>
 
 <html>
     <head>
@@ -20,21 +46,33 @@ if(!$db)
         <br><br>
         <div id="loginbox">
             <br>
+            <form name="login" id="login" action="index.php" method="post">
             <table width="80%" border="0" cellpadding="5">
                 <tr>
                     <td>Username</td>
-                    <td><input type="text" id="uname"></td>
+                    <td><input type="text" id="uname" name="uname"></td>
                 </tr>
                 <tr>
                     <td>Password</td>
-                    <td><input type="password" id="passwd"></td>
+                    <td><input type="password" id="passwd" name="passwd"></td>
                 </tr>
                 <tr>
                     <td colspan="2" align="center">
-                        <div class="btn1">Login</div>
+                        <div class="btn1" onclick="javascript:document.getElementById('login').submit()">Login</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <?php
+                        if(isset($_GET['e']))
+                        {
+                            echo "<font color=red><b>".$_GET['e']."</b></font>";
+                        }
+                        ?>
                     </td>
                 </tr>
             </table>
+            </form>
         </div>
     </center>
     </body>
