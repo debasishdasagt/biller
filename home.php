b41c4a59a8089339951cc05251b3bc9ebbf1304f
debasishdasagt <debasishdasagt@users.noreply.github.com>
2017-07-24 07:37:33 +0530

    ~

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" href="CSS/pagestyle.css">
+    </head>
+    <body>
+    <center>
+        <table width="100%" cellpadding="0" cellspacing="0" border="0">
+            <tr>
+                <td valign="bottom" align="center"><div id="topbarbg"></div>
+                
+                    <table border="0" width="900">
+                        <tr>
+                            <td valign="bottom">
+                                <table border="0">
+                                    <tr>
+                                        <td><div class="btn2">Home</div></td>
+                                        <td><div class="btn2">PNR</div></td>
+                                    </tr>
+                                </table>
+                                
+                                
+                            </td>
+                            <td align="right"><div id="black_flight"></div></td>
+                        </tr>
+                    </table>
+                
+                </td>
+            </tr>
+        </table>
+    </center>
+    </body>
+</html>
